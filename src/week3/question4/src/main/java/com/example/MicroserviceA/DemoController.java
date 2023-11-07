5221aaf2bc0e06a1ce91fc53769f85253a6d7f3a
Anmol <anmoldhall1221@gmail.com>
2023-11-07 17:04:59 +0530

    pushing question 4

--- src/week3/question4/src/main/java/com/example/MicroserviceA/DemoController.java
+++ src/week3/question4/src/main/java/com/example/MicroserviceA/DemoController.java
@@ -1,13 +1,26 @@
 package com.example.MicroserviceA;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController{
 
+    @Value("${spring.datasource.name}")
+    private String name;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
     @GetMapping("/test")
     public String testFunction(){
+        System.out.println(name);
+        System.out.println(url);
+        System.out.println(username);
         return "This is a testing Controller";
     }
 }
